cecfbedb571a44d0d875309c58d49ce89643d7d4
stkollamp <104965505+stkollamp@users.noreply.github.com>
2022-06-17 23:44:43 +0530

    Added code for  Rotating an Array

--- Arrays/Top50GFG/RotateArray.java
+++ Arrays/Top50GFG/RotateArray.java
@@ -6,19 +6,18 @@ package Arrays;
  */
 
 public class RotateArray {
-	public void rotate(int[] nums, int k) {
-        int n=nums.length;
+    public void rotate(int[] nums, int k) {
+    	int n=nums.length;
         k=k%n;
         rotateArr(nums, 0, n-k-1);
         rotateArr(nums, n-k, n-1);
         rotateArr(nums, 0, n-1);
-        
     }	
-	public void rotateArr(int nums[], int start, int end) {
-		while(start<end) {
-			int temp=nums[start];
-			nums[start++]=nums[end];
-			nums[end--]=temp;
-		}
-	}
+    public void rotateArr(int nums[], int start, int end) {
+    	while(start<end) {
+    		int temp=nums[start];
+    		nums[start++]=nums[end];
+    		nums[end--]=temp;
+    	}
+    }
 }
